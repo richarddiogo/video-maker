02dbdb8001592dd64105b5a16a4fafcb0c9ce64a
unknown <richar.emp@gmail.com>
2019-04-16 12:50:20 -0300

    Input prefix para termos de busca

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ function start() {
     const  content = {}
 
     content.searchTerminal = askAndReturnSearchTerm()
+    content.prefix = askAndReturnPrefix()
 
     function askAndReturnSearchTerm(){
         return readline.question('Termo para buscar na web: ')
+    }
+
+    function askAndReturnPrefix() { 
+        const prefixes = ['Who is', 'What is','The history of']
+        const selectPrefixIndex = readline.keyInSelect(prefixes)
+        const selectPrefixText = prefixes[selectPrefixIndex]
+        
+        return selectPrefixText
     }
 
     console.log(content)
